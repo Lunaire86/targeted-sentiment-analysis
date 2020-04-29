1c68bd1a7d316380e9b09d824f6d44dfb3c2fbfd
Maria Singstad Paulsen <fero@live.no>
2020-04-29 20:42:54 +0200

    even better handling of project related paths... lol

--- babil/utils/config.py
+++ babil/utils/config.py
@@ -1,103 +1,28 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
-import os
-import time
+import json
 from argparse import ArgumentParser, Namespace
-from dataclasses import dataclass, field, InitVar
-
-# These informational messages get printed to
-# the terminal before any of the input prompts.
-user_info = [
-    'PURPOSE: set project level variables for directories '
-    'containing i.e. word embeddings, datasets and models.\n',
-    'REASON: allow for the loading of pickled versions, if '
-    'they exist, without it having to be explicitly specified.\n'
-    'Setting these variables here takes away the need for having '
-    'them passed in as command line arguments each time.\n',
-
-    'Some user errors will have been accounted for, but probably '
-    'not all. Please keep this in mind when passing input to this script.\n',
-
-    'User input like yes and no is case-insensitive and can be shortened '
-    'to a single letter. If the expected input is a path to a file or '
-    'directory, it is case-sensitive. The user is not required to surround '
-    'these paths by single or double quotes, but it is encouraged.\n'
-]
-
-# Probably not the most elegant solution, but it sort of works
-input_prompts = {
-    'project_root': f'\n{"=" * 22}\nProject root directory\n{"=" * 22}\n'
-                    f'Is {os.getcwd()} the project root directory?\n'
-                    f'Yes  -> [ENTER]\n'
-                    f'No   -> specify path',
-
-    'word_embeddings': f'\n{"=" * 27}\nPre-trained word embeddings\n{"=" * 27}\n'
-                       f'Where should the program look for word embeddings?\n'
-                       f'project_root/models    -> [ENTER]\n'
-                       f'Saga shared directory  -> [S]\n'
-                       f'Other                  -> specify path\n'
-}
+from dataclasses import dataclass, field
 
 
 @dataclass
-class PathTo:
-    project_root: str = os.getcwd()
-    saga_shared: str = '/cluster/shared/nlpl/data/vectors/latest'
-    embeddings: str = field(init=False)
+class PathTracker:
+    project_root: str
+    data: str
+    raw_data: str
+    interim_data: str
+    processed_data: str
+    embeddings: str
+    models: str
+    train: str
+    dev: str
+    test: str
 
-    # Datasets
-    data: str = field(init=False)
-    raw_data: str = field(init=False)
-    interim_data: str = field(init=False)
-    processed_data: str = field(init=False)
-    train: str = field(init=False)
-    dev: str = field(init=False)
-    test: str = field(init=False)
-
-    SETUP: InitVar[bool] = False
-
-    def __post_init__(self, SETUP):
-        if SETUP:
-            self._run_setup()
-        else:
-            self.embeddings = os.path.join(self.project_root, 'models')
-
-        # Datasets
-        self.data = os.path.join(self.project_root, 'data')
-        self.raw_data = os.path.join(self.data, 'raw')
-        self.interim_data = os.path.join(self.data, 'interim')
-        self.processed_data = os.path.join(self.data, 'processed')
-        self.train, self.dev, self.test = [
-            os.path.join(self.raw_data, basename)
-            for basename in ('train', 'dev', 'test')
-        ]
-
-    def _run_setup(self):
-        for msg in user_info:
-            print(msg)
-            time.sleep(0.15)
-
-        # Set the different paths
-        self.project_root = self._set_path(input(input_prompts['project_root']))
-        self.embeddings = self._set_path(input(input_prompts['word_embeddings']))
-
-    def _get_abspath(self, path):
-        """Returns an absolute path."""
-        abspath = os.path.expanduser(  # expand ~/
-            os.path.expandvars(  # expand any shell variables, i.e. $SOME_PATH
-                os.path.abspath(  # get absolute (normalised) path
-                    path)))  # path from user
-        return abspath
-
-    def _set_path(self, user_input):
-        abspath = self._get_abspath(user_input)
-
-        if os.path.isdir(abspath):
-            return abspath
-
-        raise NotADirectoryError(f"User input: {user_input}\n"
-                                 f"Expanded path: {abspath}")
+    @staticmethod
+    def from_json(json_file):
+        with open(json_file, 'r') as f:
+            entries = json.load(f)
+        return PathTracker(**entries)
 
 
 @dataclass
