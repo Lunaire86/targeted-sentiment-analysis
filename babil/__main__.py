ce1adc3e24239dce0d9e8eb2995ac6c55f771924
Maria Singstad Paulsen <fero@live.no>
2020-05-02 02:10:44 +0200

    Rearranged main entry for program

--- babil/__main__.py
+++ babil/__main__.py
@@ -1,8 +1,63 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import logging
 
-import numpy as np
-import pandas as pd
+from models import baseline
+from utils.config import ArgParser, PathTracker, set_global_seed
+
+
+def dev_mode():
+    # Store command line arguments
+    parser = ArgParser()  # class defined in babil/utils/config.py
+    args = parser.args
+    print(args, sep='\n')
+
+    # Initialise a logger
+    logging.basicConfig(filename='logger.log', filemode='a',
+                        format='{asctime} : {levelname} : {message}',
+                        datefmt='%Y-%m-%d %X',
+                        style='{',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Ensure reproducibility
+    set_global_seed()
+
+    # Easy access to various file locations
+    paths = (
+        PathTracker.from_json('saga_config.json') if args.saga
+        else PathTracker.from_json('local_config.json')
+    )
+    logger.info(f'Project paths set:\n{paths}')
+
+    return args, paths, logger
+
 
 if __name__ == '__main__':
-    pass
+    # Store command line arguments
+    parser = ArgParser()  # class defined in babil/utils/config.py
+    args = parser.args
+    print(args, sep='\n')
+
+    # Initialise a logger
+    logging.basicConfig(filename='logger.log', filemode='a',
+                        format='{asctime} : {levelname} : {message}',
+                        datefmt='%Y-%m-%d %X',
+                        style='{',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Ensure reproducibility
+    set_global_seed()
+
+    # Easy access to various file locations
+    paths = (
+        PathTracker.from_json('saga_config.json') if args.saga
+        else PathTracker.from_json('local_config.json')
+    )
+    logger.info(f'Project paths set:\n{paths}')
+
+    if args.run == 'baseline':
+        s = '~' * len(args.run)
+        print(f'{s}\nBASELINE\n{s}')
+        baseline.run(args, paths, logger)
