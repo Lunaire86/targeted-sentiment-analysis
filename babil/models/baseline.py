8fa5df46065cc0c87445e072fae1618f191a640b
Maria Singstad Paulsen <fero@live.no>
2020-05-04 18:04:41 +0200

    cleaner code, vectorising implemented

--- babil/models/baseline.py
+++ babil/models/baseline.py
@@ -5,68 +5,93 @@ import os
 import pickle
 from argparse import Namespace
 from logging import Logger
+from os.path import join
+from typing import Union
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Bidirectional, Dense, Dropout, Embedding
-from tensorflow.keras.layers import Input, LSTM, LSTMCell, Masking
-from tensorflow.keras.losses import CategoricalCrossentropy, SparseCategoricalCrossentropy
-from tensorflow.keras.optimizers import Adamax, Adam
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from gensim.models import FastText
+from gensim.models.fasttext import FastTextKeyedVectors, load_facebook_model, load_facebook_vectors
 
-from data.preprocessing import ConllData, Dataset, LabelTokeniser, WordTokeniser
-from features.embeddings import load_embeddings
-from sandbox.data_generator import Minimal
-from utils.config import ArgParser, PathTracker, set_global_seed
-from utils.helpers import save_pickle
+from data.preprocessing import Dataset, LabelTokeniser, WordTokeniser, vectorise
+from utils.config import PathTracker
 
 
-def pickle_me_elmo(embeddings, vocab, path_to, activate=False):
-    # Datasets get pickled automatically, but we
-    # need to pickle vocab and embeddings manually
-    if activate:
-        print('Pickling embeddings and vocab...')
-        save_pickle(embeddings, path_to.models)
-        save_pickle(vocab, path_to.data)
-        print('Done!')
+def throwaway():
+    pass
 
 
 def run(parsed_args: Namespace, paths: PathTracker, logger: Logger):
+    # Based on checking the length of all sentences in train and dev,
+    # setting max sequence length to 50 seems reasonable
+    MAX_SEQUENCE_LENGTH = 50
+
     args = parsed_args
     path_to = paths
 
-    # Load datasets
-    train = ConllData(path_to.train)
-    dev = ConllData(path_to.dev)
-    test = ConllData(path_to.test)  # Keep ur hands off, bro
+    print('Loading the dataset...')
+    train = Dataset(path_to.train)
+    dev = Dataset(path_to.dev)
+    test = Dataset(path_to.test)  # Keep ur hands off, bro
 
-    # Load embeddings
     print('Loading pre-trained embeddings...')
-    embeddings = np.load(os.path.join(path_to.models, 'small-vec.npy'))
-    # embeddings = load_embeddings(args.embedding_id, path_to)
-    # small_embeddings = embeddings.__dummy__()
-    print(f'Embeddings shape: {embeddings.shape}')
-    # print(f', dummy shape: {small_embeddings.shape}')
+    basename: str = ''
+    embeddings: Union[FastText, FastTextKeyedVectors] = None
+    if args.saga:
+        basename = 'cc.no.300.bin'
+        path_to_embeddings = join(path_to.models, basename)  # saga
+        embeddings = load_facebook_model(path_to_embeddings, encoding='latin1')
+    else:
+        basename = 'cc.no.20.bin'
+        path_to_embeddings = join(path_to.embeddings, 'norwegian', basename)
+        embeddings = load_facebook_vectors(path_to_embeddings, encoding='latin1')
 
-    # Create shared vocabulary for tasks
+    # Get the weights and add vectors representing
+    # indices 0 and 1 by concatenating along the first
+    # axis (i.e. 'inserting' two rows at the front)
+    embedding_dim = int(basename.split('.')[2])
+    pad_vec = tf.random.uniform(shape=[1, embedding_dim], minval=0.05, maxval=0.95, seed=69686)
+    unk_vec = tf.random.uniform(shape=[1, embedding_dim], minval=0.05, maxval=0.95, seed=69686)
+    weights = np.r_[pad_vec.numpy(), unk_vec.numpy(), embeddings.vectors]
+
+    # Update the vocab with tokens for padding and unknown words
+    embeddings_vocab = embeddings.index2entity
+    word2idx = {'<PAD>': 0, '<UNK>': 1}
+    word2idx.update(
+        {word: idx + 2 for (idx, word) in enumerate(embeddings_vocab)}
+    )
+    idx2word = {idx: word for (word, idx) in word2idx.items()}
+
+    # Save the word-to-index dict
+    w2i_path = join(path_to.models, f'{basename}_word2idx.pickle')
+    mode = 'wb' if os.path.exists(w2i_path) else 'xb'
+    with open(w2i_path, mode) as f:
+        pickle.dump(word2idx, f)
+
+    # And also save the weights using numpy
+    npy_path = join(path_to.models, f'{basename}_weights.npy')
+    mode = 'wb' if os.path.exists(npy_path) else 'xb'
+    with open(npy_path, mode) as f:
+        np.save(f, weights)
+
     print('Building the vocabulary...')
-    vocab = None
-    with open(os.path.join(path_to.data, 'Vocab.pickle'), 'rb') as f:
-        vocab = pickle.load(f)
-    # vocab = Vocab()
-    labels = train.get_labels()
+    # Create tokenisers
+    label_tokeniser = LabelTokeniser()
+    word_tokeniser = WordTokeniser()
 
-    # Add words from both word embeddings and our training data
-    # vocab.add(embeddings.vocab)
-    # vocab.add(train.get_vocab())
-    print('Done!')
+    # Fit to features and labels
+    word_tokeniser.fit_on_texts(train.X)
+    label_tokeniser.fit_on_texts(train.y)
 
-    # Convert from ConllData to Dataset
-    train_ds = Data(train, vocab)
-    dev_ds = Data(dev, vocab)
+    # Save them as serialised objects using pickle
+    word_tokeniser.save(path_to.interim_data)
+    label_tokeniser.save(path_to.interim_data)
 
-    pickle_me_elmo(embeddings, vocab, path_to)
+    X_train = ''
+    X_dev = ''
 
+    y_train = ''
+    y_dev = ''
 
     # Create the embedding layer
     # embedding_layer = Embedding(
@@ -90,28 +115,61 @@ def run(parsed_args: Namespace, paths: PathTracker, logger: Logger):
 
 
 if __name__ == '__main__':
+    # Based on checking the length of all sentences in train and dev,
+    # setting max sequence length to 50 seems reasonable
+    MAX_SEQUENCE_LENGTH = 50
+
+    # Setup for running locally, as opposed to on Saga
     from babil.__main__ import dev_mode
+
     args, path_to, logger = dev_mode()
 
-    # Load datasets
+    print('Loading the dataset...')
     train = Dataset(path_to.train)
     dev = Dataset(path_to.dev)
     test = Dataset(path_to.test)  # Keep ur hands off, bro
 
-    # Create tokenisers
-    label_tokeniser = LabelTokeniser()
+    # We will just load the pickled files here
+    # See run() for implementation of loading the actual model
+    print('Loading pre-trained embeddings...')
+    npy_path = join(path_to.models, 'cc.no.20.bin_weights.npy')
+    w2i_path = join(path_to.models, 'cc.no.20.bin_word2idx.pickle')
+    f = open(w2i_path, 'rb')
+
+    weights = np.load(npy_path)
+    word2idx = pickle.load(f)
+
+    idx2word = {idx: word for (word, idx) in word2idx.items()}
+    f.close()
+
+    print('Building the vocabulary...')
+    # Words
     word_tokeniser = WordTokeniser()
-
-    # Fit to features and labels
     word_tokeniser.fit_on_texts(train.X)
-    label_tokeniser.fit_on_texts(train.y)
-
-    # Save them as serialised objects using pickle
     word_tokeniser.save(path_to.interim_data)
-    label_tokeniser.save(path_to.interim_data)
+    print(f'Found {len(word_tokeniser.word_index)} different words.')
 
-    # TODO : start here - build shared vocab
-    ...
+    # Labels
+    label_tokeniser = LabelTokeniser()
+    label_tokeniser.fit_on_texts(train.y)
+    label_tokeniser.save(path_to.interim_data)
+    print(f'Found {len(label_tokeniser.word_index)} different labels.')
+
+    # Vectorise words
+    X_train = vectorise(train.X, word2idx=word2idx)
+    X_dev = vectorise(dev.X, word2idx=word2idx)
+
+    # Vectorise labels
+    y_train = vectorise(train.y, label_tokeniser, categorical=True)
+    y_dev = vectorise(dev.y, label_tokeniser, categorical=True)
+
+    # Create shared vocabulary for tasks
+    # vocab = None
+    # with open(os.path.join(path_to.data, 'Vocab.pickle'), 'rb') as f:
+    #     vocab = pickle.load(f)
+    # # vocab = Vocab()
+    # labels = train.get_labels()
+
     # Load embeddings
     # print('Loading pre-trained embeddings...')
     # embeddings = np.load(os.path.join(path_to.models, 'small-vec.npy'))
@@ -120,13 +178,6 @@ if __name__ == '__main__':
     # print(f'Embeddings shape: {embeddings.shape}')
     # # print(f', dummy shape: {small_embeddings.shape}')
     #
-    # # Create shared vocabulary for tasks
-    # print('Building the vocabulary...')
-    # vocab = None
-    # with open(os.path.join(path_to.data, 'Vocab.pickle'), 'rb') as f:
-    #     vocab = pickle.load(f)
-    # # vocab = Vocab()
-    # labels = train.get_labels()
     #
     # # Add words from both word embeddings and our training data
     # # vocab.add(embeddings.vocab)
