5e6c358c6ec792e1c1c6bd2b70953de15fcb620f
Maria Singstad Paulsen <fero@live.no>
2020-05-09 16:33:20 +0200

    support for training domain-specific word-embeddings

---
+++ conllu_to_tsv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# find . -iname '*.doc' -exec echo "File is {}" \;
+
+# Usage: cat path_to.conllu | ./conllu_to_tsv.sh > path_to.tsv
+#printf "form\n"
+
+grep -v '^#' | awk -F'\t' -v form='' \
+    '{ORS=" "} {if ($1 ~ /^$/) {printf "%s\n", form; form=""} else {form = form $2 " "}}' | sed 's/ \t/\t/g'
