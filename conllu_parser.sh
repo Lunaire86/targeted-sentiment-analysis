85370573636099eab40eef920f5ca6db9096d098
Maria Singstad Paulsen <fero@live.no>
2020-05-11 18:29:38 +0200

    minor edits to shell scripts

--- conllu_parser.sh
+++ conllu_parser.sh
@@ -2,6 +2,6 @@
 
 ## Parse CoNNL-U files to cor, txt or other format.
 
-find . -name "*.conllu" -exec sh -c "cat {} | ./conllu_to_tsv.sh > {}.cor" \;
+find /cluster/work/users/marispau/nnc/ -name "avis-rest.s" -exec sh -c "cat {} | /cluster/work/users/marispau/in5550/sent_detagger.sh > {}.cor" \;
 
 
