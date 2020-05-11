e53310857607afde9093f939f37cef6f5497a49d
Maria Singstad Paulsen <fero@live.no>
2020-05-11 12:36:04 +0200

    fixed a bug causing the logger not to work

--- coleus-1.0/coleus/__main__.py
+++ coleus-1.0/coleus/__main__.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from time import time
-from logging import Logger
+import logging
+import os
 from argparse import Namespace
+from logging import Logger
 from os import path
+from time import time
 from zipfile import ZipFile
 
 from src.model import Model
-from src.utils import get_filenames, ArgParser, get_logger, log_msg
+from src.utils import get_filenames, ArgParser
+
 
 if __name__ == '__main__':
     # Type declarations
@@ -26,9 +29,17 @@ if __name__ == '__main__':
     model_path = path.join(args.target, model_file)
 
     # Create a logger and print the path to the log file
-    logger = None if args.quiet else get_logger(log_file)
-    log_msg(f'Program arguments:\n{vars(args)}', logger)
-    print(f'Log file: {log_file if logger else None}')
+    logging.basicConfig(
+        filename=log_file,
+        filemode='a',
+        format='{asctime} : {levelname} : {message}',
+        datefmt='%Y-%m-%d %X',
+        style='{',
+        level=logging.INFO
+    )
+    logger = logging.getLogger(__name__)
+    logger.info(f'Program arguments:\n{vars(args)}')
+    print(f'Log file: {os.path.abspath(log_file)}')
 
     # Initialise (build) the model.
     model = Model.init(args)
@@ -37,13 +48,13 @@ if __name__ == '__main__':
     # model.build_vocab(more_training_data, update=True)
     # before moving on to the training.
 
-    log_msg('Training...')
+    logger.info('Training...')
     t = time()
     model.train(args.source)
     training_time = time() - t
 
-    log_msg(f'Finished training in {training_time} seconds (~{int(training_time / 60)} minute(s)).')
-    log_msg(model.__str__())
+    logger.info(f'Finished training in {training_time} seconds (~{int(training_time / 60)} minute(s)).')
+    logger.info(model.__str__())
     print(model)
 
     mode = 'wb' if path.exists(model_path) else 'xb'
@@ -51,9 +62,9 @@ if __name__ == '__main__':
         with ZipFile(model_path, mode[0]) as archive:
             archive.write(filename=model.save(), arcname='model.bin')
             archive.write(filename=model.wv, arcname='model.vec')
-            log_msg(f'Saved to archive {model_path} -> model.bin, model.vec')
+            logger.info(f'Saved to archive {model_path} -> model.bin, model.vec')
     else:
         with open(model_path, mode) as f:
             model.save(f)
-        log_msg(f'Saved to file -> {model_path}')
+        logger.info(f'Saved to file -> {model_path}')
 
