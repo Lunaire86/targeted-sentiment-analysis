57eb4f2b821e6e25fa0ab131bcdeb463a8c98dd7
Maria Singstad Paulsen <fero@live.no>
2020-05-13 01:29:43 +0200

    tidied up for pushing to UiO repo

--- conllu_parser.sh
+++ conllu_parser.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 ## Parse CoNNL-U files to cor, txt or other format.
+# Copy the uncommented line below and run it in the terminal
+# to pipe through a bunch of conll/conllu files in one go.
 
-find /cluster/work/users/marispau/nnc/ -name "avis-rest.s" -exec sh -c "cat {} | /cluster/work/users/marispau/in5550/sent_detagger.sh > {}.cor" \;
+#find entry/point/ -name "*.conllu" -exec sh -c "cat {} | conllu_parser.sh > {}.cor" \;
+
+
+grep -v '^#' | awk -F'\t' -v form='' \
+    '{ORS=" "} {if ($1 ~ /^$/) {printf "%s\n", form; form=""} else {form = form $2 " "}}' | sed 's/ \t/\t/g'
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='babil',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.9.0',
     description='IN5550 Home Exam Project - Spring 2020',
     author='Maria Singstad Paulsen',
     license='MIT',
